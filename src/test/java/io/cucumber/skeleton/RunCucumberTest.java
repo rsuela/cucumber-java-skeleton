e758fedd6c84a9154a4120dbf5c90709404db38a
Rey Suela <Rey.Suela@ird.govt.nz>
2020-02-10 11:40:13 +1300

    modified the jenkinsfile to run gradle

--- src/test/java/io/cucumber/skeleton/RunCucumberTest.java
+++ src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -5,6 +5,8 @@ import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(
+     plugin = {"pretty", "json:build/test-results/test/Cucumber.json"}
+  )
 public class RunCucumberTest {
 }
